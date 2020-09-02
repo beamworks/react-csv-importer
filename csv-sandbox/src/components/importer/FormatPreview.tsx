b1c299c3e09a385c3b80561da3f4add64a5c44f0
Nick Matantsev <nick.matantsev@gmail.com>
2020-09-01 21:12:22 -0400

    Implement async preview parsing

--- csv-sandbox/src/components/importer/FormatPreview.tsx
+++ csv-sandbox/src/components/importer/FormatPreview.tsx
@@ -1,9 +1,65 @@
-import React, { useCallback, useRef, useEffect } from 'react';
+import React, { useCallback, useRef, useEffect, useState } from 'react';
 import Papa from 'papaparse';
 import { makeStyles } from '@material-ui/core/styles';
 import Link from '@material-ui/core/Link';
 
 const useStyles = makeStyles((theme) => ({}));
+
+const MAX_ROWS = 5;
+
+type PreviewResults =
+  | {
+      parseError: undefined;
+      parseWarning?: Papa.ParseError;
+      firstChunk: string;
+      firstRows: unknown[][];
+    }
+  | {
+      parseError: Papa.ParseError;
+    };
+
+function parsePreview(file: File): Promise<PreviewResults> {
+  return new Promise((resolve) => {
+    let firstChunk: string | null = null;
+    let firstWarning: Papa.ParseError | undefined = undefined;
+    const rowAccumulator: unknown[][] = [];
+
+    function reportSuccess() {
+      resolve({
+        parseError: undefined,
+        parseWarning: firstWarning || undefined,
+        firstChunk: firstChunk || '',
+        firstRows: rowAccumulator
+      });
+    }
+
+    Papa.parse(file, {
+      preview: MAX_ROWS,
+      skipEmptyLines: true,
+      error: (error) => {
+        resolve({
+          parseError: error
+        });
+      },
+      beforeFirstChunk: (chunk) => {
+        firstChunk = chunk;
+      },
+      step: ({ data, errors }) => {
+        rowAccumulator.push(data);
+
+        if (errors.length > 0 && firstWarning === null) {
+          firstWarning = errors[0];
+        }
+
+        // finish if parsing stops here (PP does not call done then)
+        if (rowAccumulator.length === MAX_ROWS) {
+          reportSuccess();
+        }
+      },
+      complete: reportSuccess
+    });
+  });
+}
 
 export const FormatPreview: React.FC<{ file: File; onCancel: () => void }> = ({
   file,
@@ -21,20 +77,26 @@ export const FormatPreview: React.FC<{ file: File; onCancel: () => void }> = ({
     []
   );
 
-  useEffect(() => {
-    // ignore if nothing to do
-    if (file === null) {
-      return;
-    }
+  const [preview, setPreview] = useState<PreviewResults | null>(null);
 
-    Papa.parse(file, {
-      preview: 5,
-      skipEmptyLines: true,
-      error: (error) => console.error('PapaParse error', error.message),
-      complete: function (results) {
-        console.log(results);
+  // perform async preview parse
+  const asyncLockRef = useRef<number>(0);
+  useEffect(() => {
+    const oplock = asyncLockRef.current;
+
+    parsePreview(file).then((results) => {
+      // ignore if stale
+      if (oplock !== asyncLockRef.current) {
+        return;
       }
+
+      setPreview(results);
     });
+
+    return () => {
+      // invalidate current oplock on change or unmount
+      asyncLockRef.current += 1;
+    };
   }, [file]);
 
   return (
@@ -43,6 +105,14 @@ export const FormatPreview: React.FC<{ file: File; onCancel: () => void }> = ({
       <Link href="#" onClick={cancelClickHandler}>
         Go back
       </Link>
+      {preview === null ? (
+        <div>Parsing...</div>
+      ) : (
+        <div>
+          Data preview:
+          <pre>{!preview.parseError && preview.firstChunk.slice(0, 100)}</pre>
+        </div>
+      )}
     </div>
   );
 };
