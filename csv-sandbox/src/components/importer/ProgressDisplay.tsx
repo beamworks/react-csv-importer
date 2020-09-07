f65aa0f2b5147d8e71abd21d6a9cc09d87c5f6b1
Nick Matantsev <nick.matantsev@gmail.com>
2020-09-07 13:55:28 -0400

    Show asymptotic progress bar

--- csv-sandbox/src/components/importer/ProgressDisplay.tsx
+++ csv-sandbox/src/components/importer/ProgressDisplay.tsx
@@ -1,15 +1,45 @@
-import React from 'react';
+import React, { useState, useEffect, useMemo } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
+import Typography from '@material-ui/core/Typography';
+import LinearProgress from '@material-ui/core/LinearProgress';
 
 import { PreviewInfo } from './FormatPreview';
 import { ImporterFrame } from './ImporterFrame';
 
-const useStyles = makeStyles((theme) => ({}));
+const useStyles = makeStyles((theme) => ({
+  progressFrame: {
+    padding: theme.spacing(4)
+  }
+}));
+
+const estimatedTotal = 100;
 
 export const ProgressDisplay: React.FC<{
   preview: PreviewInfo;
 }> = ({ preview }) => {
   const styles = useStyles();
+
+  const [progressCount, setProgressCount] = useState(0);
+
+  useEffect(() => {
+    const intervalId = window.setInterval(() => {
+      setProgressCount((prev) => prev + 10);
+    }, 400);
+
+    return () => {
+      clearInterval(intervalId);
+    };
+  }, []);
+
+  // simulate asymptotic progress percentage
+  const progressPercentage = useMemo(() => {
+    // inputs hand-picked so that correctly estimated total is about 65% of the bar
+    const progressPower = 1.5 * (progressCount / estimatedTotal);
+    const progressLeft = 0.5 ** progressPower;
+
+    // convert to .1 percent precision for smoother bar display
+    return Math.floor(1000 - 1000 * progressLeft) / 10;
+  }, [progressCount]);
 
   return (
     <ImporterFrame
@@ -21,7 +51,12 @@ export const ProgressDisplay: React.FC<{
         // @todo
       }}
     >
-      Please wait...
+      <div className={styles.progressFrame}>
+        <Typography variant="body2" color="textSecondary" align="right">
+          {progressCount}
+        </Typography>
+        <LinearProgress variant="determinate" value={progressPercentage} />
+      </div>
     </ImporterFrame>
   );
 };
