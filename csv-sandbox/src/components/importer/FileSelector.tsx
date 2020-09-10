0b012c261c124775458a6e866a5dd7e49d38bc87
Nick Matantsev <nick.matantsev@gmail.com>
2020-09-09 21:35:36 -0400

    Start converting importer to be MUI-agnostic

--- csv-sandbox/src/components/importer/FileSelector.tsx
+++ csv-sandbox/src/components/importer/FileSelector.tsx
@@ -1,28 +1,11 @@
 import React, { useCallback, useRef } from 'react';
 import { useDropzone } from 'react-dropzone';
-import { makeStyles } from '@material-ui/core/styles';
 
-const useStyles = makeStyles((theme) => ({
-  dropZone: {
-    border: `4px dashed ${theme.palette.primary.main}`,
-    borderRadius: 5,
-    background: theme.palette.grey.A100,
-    padding: theme.spacing(8),
-    textAlign: 'center',
-
-    '&[data-active=true]': {
-      background: theme.palette.secondary.light,
-      color: theme.palette.secondary.contrastText,
-      transition: 'background 0.1s ease-out'
-    }
-  }
-}));
+import './FileSelector.scss';
 
 export const FileSelector: React.FC<{ onSelected: (file: File) => void }> = ({
   onSelected
 }) => {
-  const styles = useStyles();
-
   const onSelectedRef = useRef(onSelected);
   onSelectedRef.current = onSelected;
 
@@ -42,14 +25,14 @@ export const FileSelector: React.FC<{ onSelected: (file: File) => void }> = ({
 
   return (
     <div
-      className={styles.dropZone}
+      className="FileSelector"
       data-active={!!isDragActive}
       {...getRootProps()}
     >
       <input {...getInputProps()} />
 
       {isDragActive ? (
-        <span>Drop the files here ...</span>
+        <span>Drop the files here...</span>
       ) : (
         <span>Drag-and-drop some files here, or click to select files</span>
       )}
