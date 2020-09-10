7a2646146acf77f9b6717399511ee54b55cfe441
Nick Matantsev <nick.matantsev@gmail.com>
2020-09-10 00:24:33 -0400

    Reduce MUI usage

--- csv-sandbox/src/components/importer/FormatRawPreview.tsx
+++ csv-sandbox/src/components/importer/FormatRawPreview.tsx
@@ -1,35 +1,8 @@
 import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
 
 import { FormatErrorMessage } from './FormatErrorMessage';
 
-const useStyles = makeStyles((theme) => ({
-  rawPreview: {},
-  rawPreviewScroll: {
-    marginBottom: theme.spacing(2),
-    height: theme.spacing(12),
-    borderRadius: theme.shape.borderRadius,
-    background: theme.palette.primary.dark,
-    color: theme.palette.primary.contrastText,
-    overflow: 'auto'
-  },
-  rawPreviewPre: {
-    margin: 0, // override default
-    padding: `${theme.spacing(1)}px ${theme.spacing(2)}px`,
-    fontSize: theme.typography.fontSize,
-
-    '& > aside': {
-      display: 'inline-block',
-      marginLeft: theme.spacing(0.5),
-      padding: `0 0.25em`,
-      borderRadius: theme.shape.borderRadius / 2,
-      background: theme.palette.primary.contrastText,
-      color: theme.palette.primary.dark,
-      fontSize: '0.75em',
-      opacity: 0.75
-    }
-  }
-}));
+import './FormatRawPreview.scss';
 
 const RAW_PREVIEW_SIZE = 500;
 
@@ -39,14 +12,13 @@ export const FormatRawPreview: React.FC<{
   onCancelClick: (event: React.MouseEvent<HTMLButtonElement>) => void;
   // eslint-disable-next-line react/display-name
 }> = React.memo(({ chunk, warning, onCancelClick }) => {
-  const styles = useStyles();
   const chunkSlice = chunk.slice(0, RAW_PREVIEW_SIZE);
   const chunkHasMore = chunk.length > RAW_PREVIEW_SIZE;
 
   return (
-    <div className={styles.rawPreview}>
-      <div className={styles.rawPreviewScroll}>
-        <pre className={styles.rawPreviewPre}>
+    <div className="FormatRawPreview">
+      <div className="FormatRawPreview__scroll">
+        <pre className="FormatRawPreview__pre">
           {chunkSlice}
           {chunkHasMore && <aside>...</aside>}
         </pre>
