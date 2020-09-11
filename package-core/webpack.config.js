9684e7763ced3754007ec07c29efd5c57da103be
Nick Matantsev <nick.matantsev@gmail.com>
2020-09-11 17:12:13 -0400

    Move importer widget files into dedicated package folder

---
+++ package-core/webpack.config.js
@@ -0,0 +1,46 @@
+const path = require('path');
+const webpack = require('webpack');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const Dotenv = require('dotenv-webpack');
+
+module.exports = (env, argv) => ({
+  entry: './src/index.ts',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'index.[contenthash].js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: [
+          {
+            loader: 'ts-loader',
+            options: {
+              compilerOptions: {
+                noEmit: false
+              }
+            }
+          }
+        ],
+        exclude: /node_modules/
+      },
+      { test: /\.scss$/, use: ['style-loader', 'css-loader', 'sass-loader'] }
+    ]
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js']
+  },
+  devtool: 'eval-cheap-module-source-map',
+  plugins: [
+    // similar env file logic to create-react-app
+    // (using webpack mode if no env is set)
+    new Dotenv({
+      systemvars: true,
+      path: `.env.${process.env.NODE_ENV || argv.mode || 'production'}`,
+      defaults: '.env'
+    }),
+
+    new CleanWebpackPlugin()
+  ]
+});
