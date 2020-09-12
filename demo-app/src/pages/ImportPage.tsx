a0f808743b006f9ffe39e8b1a177e17bb952edcc
Nick Matantsev <nick.matantsev@gmail.com>
2020-09-12 19:26:20 -0400

    Add chart stub

--- demo-app/src/pages/ImportPage.tsx
+++ demo-app/src/pages/ImportPage.tsx
@@ -1,10 +1,24 @@
 import React from 'react';
+import { VictoryTheme, VictoryChart, VictoryArea, VictoryAxis } from 'victory';
 import Typography from '@material-ui/core/Typography';
 import { Importer, ImporterField } from 'react-csv-importer';
+
+const sampleData = [
+  { x: 1, y: 2 },
+  { x: 2, y: 3 },
+  { x: 3, y: 5 },
+  { x: 4, y: 4 },
+  { x: 5, y: 7 }
+];
 
 export const ImportPage: React.FC = () => {
   return (
     <div>
+      <VictoryChart theme={VictoryTheme.material} width={800} height={150}>
+        <VictoryArea data={sampleData} />
+        <VictoryAxis />
+      </VictoryChart>
+
       <Typography variant="body1">Import interface page.</Typography>
 
       <Importer<{ country: string }>
