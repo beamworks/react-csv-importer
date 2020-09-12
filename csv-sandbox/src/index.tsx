c4000b231b738c0ebec438234221ba586c626654
Nick Matantsev <nick.matantsev@gmail.com>
2020-09-12 01:39:02 -0400

    Link from sample code to library package

--- csv-sandbox/src/index.tsx
+++ csv-sandbox/src/index.tsx
@@ -4,5 +4,6 @@ import ReactDOM from 'react-dom';
 import { App } from './App';
 
 import './index.scss';
+import 'react-csv-importer/dist/index.css';
 
 ReactDOM.render(<App />, document.getElementById('root'));
