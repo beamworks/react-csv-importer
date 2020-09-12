f4eef4cf7878fc43546827283957329a167efbca
Nick Matantsev <nick.matantsev@gmail.com>
2020-09-12 01:44:02 -0400

    Simplify sample app compilation dependencies

--- csv-sandbox/src/index.tsx
+++ csv-sandbox/src/index.tsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 
 import { App } from './App';
 
-import './index.scss';
+import './index.css';
 import 'react-csv-importer/dist/index.css';
 
 ReactDOM.render(<App />, document.getElementById('root'));
