6bc38bb29b0c332e4fe8530ab521e0c1e79e4cf2
Nick Matantsev <nick.matantsev@gmail.com>
2020-09-12 23:33:08 -0400

    Implement dummy chart data upload

--- demo-app/src/pages/ImportPage.tsx
+++ demo-app/src/pages/ImportPage.tsx
@@ -1,42 +1,48 @@
-import React from 'react';
-import { VictoryTheme, VictoryChart, VictoryArea, VictoryAxis } from 'victory';
+import React, { useState } from 'react';
+import {
+  VictoryTheme,
+  VictoryChart,
+  VictoryStack,
+  VictoryArea,
+  VictoryAxis
+} from 'victory';
 import Typography from '@material-ui/core/Typography';
 import { Importer, ImporterField } from 'react-csv-importer';
 
-const sampleData = [
-  { x: 1, y: 2 },
-  { x: 2, y: 3 },
-  { x: 3, y: 5 },
-  { x: 4, y: 4 },
-  { x: 5, y: 7 }
-];
-
 export const ImportPage: React.FC = () => {
+  const [data, setData] = useState<{ i: number; a: number; b: number }[]>([]);
+
   return (
     <div>
       <VictoryChart theme={VictoryTheme.material} width={800} height={150}>
-        <VictoryArea data={sampleData} />
+        <VictoryStack>
+          <VictoryArea data={data} x="i" y="a" />
+          <VictoryArea data={data} x="i" y="b" />
+        </VictoryStack>
         <VictoryAxis />
       </VictoryChart>
 
       <Typography variant="body1">Import interface page.</Typography>
 
-      <Importer<{ country: string }>
+      <Importer<{ a: string; b: string }>
         processChunk={(rows) => {
-          console.log(rows);
+          setData((prev) => [
+            ...prev,
+            ...rows.map((row, rowIndex) => ({
+              i: prev.length + rowIndex,
+              a: parseInt(row.a, 10) || 0,
+              b: parseInt(row.b, 10) || 0
+            }))
+          ]);
           return new Promise((resolve) => setTimeout(resolve, 1500));
         }}
         onFinish={() => {
           console.log('dismissed!');
         }}
       >
-        <ImporterField name="name" label="Name" />
-        <ImporterField name="email" label="Email" />
-        <ImporterField name="dob" label="DOB" />
-        <ImporterField name="postalCode" label="Postal Code" />
+        <ImporterField name="a" label="Value A" />
+        <ImporterField name="b" label="Value B" />
         <ImporterField name="snack" label="Snack Preference" optional />
-        <ImporterField name="country" label="Country" optional />
-        <ImporterField name="bees" label="Bees?" />
       </Importer>
     </div>
   );
