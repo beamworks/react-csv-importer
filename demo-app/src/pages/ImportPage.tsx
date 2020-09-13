cee8cfbc632c1c97db08e6de596918008b4be74c
Nick Matantsev <nick.matantsev@gmail.com>
2020-09-12 23:40:37 -0400

    Improve sample upload processing logic

--- demo-app/src/pages/ImportPage.tsx
+++ demo-app/src/pages/ImportPage.tsx
@@ -6,7 +6,9 @@ import {
   VictoryArea,
   VictoryAxis
 } from 'victory';
+import Box from '@material-ui/core/Box';
 import Typography from '@material-ui/core/Typography';
+import Paper from '@material-ui/core/Paper';
 import { Importer, ImporterField } from 'react-csv-importer';
 
 export const ImportPage: React.FC = () => {
@@ -22,28 +24,42 @@ export const ImportPage: React.FC = () => {
         <VictoryAxis />
       </VictoryChart>
 
-      <Typography variant="body1">Import interface page.</Typography>
+      <Paper>
+        <Box px={2} py={1}>
+          <Typography variant="body1">
+            Upload any CSV file with one or two numeric columns, and it will be
+            graphed on the above chart.
+          </Typography>
+        </Box>
+      </Paper>
 
-      <Importer<{ a: string; b: string }>
-        processChunk={(rows) => {
-          setData((prev) => [
-            ...prev,
-            ...rows.map((row, rowIndex) => ({
-              i: prev.length + rowIndex,
-              a: parseInt(row.a, 10) || 0,
-              b: parseInt(row.b, 10) || 0
-            }))
-          ]);
-          return new Promise((resolve) => setTimeout(resolve, 1500));
-        }}
-        onFinish={() => {
-          console.log('dismissed!');
-        }}
-      >
-        <ImporterField name="a" label="Value A" />
-        <ImporterField name="b" label="Value B" />
-        <ImporterField name="snack" label="Snack Preference" optional />
-      </Importer>
+      <Box mt={2}>
+        <Importer<{ a: string; b: string }>
+          processChunk={(rows) => {
+            setData(
+              (prev) =>
+                [
+                  ...prev,
+                  ...rows.map((row, rowIndex) => ({
+                    i: prev.length + rowIndex,
+                    a: parseInt(row.a, 10) || 0,
+                    b: row.b === undefined ? 0 : parseInt(row.b, 10) || 0
+                  }))
+                ].slice(-200) // limit total displayed
+            );
+
+            // artificial delay
+            return new Promise((resolve) => setTimeout(resolve, 500));
+          }}
+          onFinish={() => {
+            console.log('dismissed!');
+          }}
+        >
+          <ImporterField name="a" label="Value A" />
+          <ImporterField name="b" label="Value B" optional />
+          <ImporterField name="snack" label="Snack Preference" optional />
+        </Importer>
+      </Box>
     </div>
   );
 };
