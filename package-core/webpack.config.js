11a3f4633a81be576bf81ed65e049e83a8587f96
Nick Matantsev <nick.matantsev@gmail.com>
2021-01-21 23:55:39 -0500

    Tweak TS config to allow test vs build versions

--- package-core/webpack.config.js
+++ package-core/webpack.config.js
@@ -18,6 +18,7 @@ module.exports = (env, argv) => ({
           {
             loader: 'ts-loader',
             options: {
+              configFile: 'tsconfig.base.json',
               compilerOptions: {
                 noEmit: false
               }
