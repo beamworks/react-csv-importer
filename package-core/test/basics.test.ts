fdb2e313ddd2eada2d4a6006a937d165d8aeedeb
Nick Matantsev <nick.matantsev@gmail.com>
2021-01-22 00:42:46 -0500

    Add test asset compilation helper

--- package-core/test/basics.test.ts
+++ package-core/test/basics.test.ts
@@ -1,8 +1,10 @@
 import { expect } from 'chai';
 
+import { runTestServer } from './testServer';
 import { runDriver } from './webdriver';
 
 describe('importer basics', () => {
+  const appUrl = runTestServer();
   const getDriver = runDriver();
 
   it('adds 2 and 2', () => {
