fffbea0442c2c243acc05beb28a74fdaa05ea22a
Nick Matantsev <nick.matantsev@gmail.com>
2021-01-23 13:09:05 -0500

    Instantiate importer component in test browser

--- package-core/test/basics.test.ts
+++ package-core/test/basics.test.ts
@@ -1,6 +1,7 @@
 import { expect } from 'chai';
 import ReactModule from 'react';
 import ReactDOMModule from 'react-dom';
+import { ImporterProps } from '../src/components/ImporterProps';
 
 import { runTestServer } from './testServer';
 import { runDriver } from './webdriver';
@@ -10,17 +11,33 @@ describe('importer basics', () => {
   const getDriver = runDriver();
 
   async function runScript(
-    script: (r: typeof ReactModule, rd: typeof ReactDOMModule) => void
+    script: (
+      r: typeof ReactModule,
+      rd: typeof ReactDOMModule,
+      im: (
+        props: ImporterProps<Record<string, unknown>>
+      ) => ReactModule.ReactElement
+    ) => void
   ) {
-    await getDriver().executeScript(`(${script.toString()})(React, ReactDOM)`);
+    await getDriver().executeScript(
+      `(${script.toString()})(React, ReactDOM, ReactCSVImporter.Importer)`
+    );
   }
 
   it('adds 2 and 2', async () => {
     await getDriver().get(appUrl);
 
-    await runScript((React, ReactDOM) => {
+    await runScript((React, ReactDOM, ReactCSVImporter) => {
       ReactDOM.render(
-        React.createElement('div', {}, ['Hello world']),
+        React.createElement(
+          ReactCSVImporter,
+          {
+            processChunk: (rows) => {
+              console.log('chunk', rows);
+            }
+          },
+          []
+        ),
         document.getElementById('root')
       );
     });
