4f8024f14c91977c94919837f3d2d40f61e6c84b
Nick Matantsev <nick.matantsev@gmail.com>
2021-01-26 23:22:17 -0500

    Add file preview test steps

--- package-core/test/basics.test.ts
+++ package-core/test/basics.test.ts
@@ -66,17 +66,88 @@ describe('importer basics', () => {
     expect(await fileInput.getAttribute('type')).to.equal('file');
   });
 
-  it('accepts file', async () => {
-    const filePath = path.resolve(__dirname, './fixtures/simple.csv');
+  describe('with file selected', () => {
+    beforeEach(async () => {
+      const filePath = path.resolve(__dirname, './fixtures/simple.csv');
 
-    const fileInput = await getDriver().findElement(By.xpath('//input'));
-    fileInput.sendKeys(filePath);
+      const fileInput = await getDriver().findElement(By.xpath('//input'));
+      fileInput.sendKeys(filePath);
 
-    await getDriver().wait(
-      until.elementLocated(
-        By.xpath('//span[contains(., "Raw File Contents")]')
-      ),
-      300 // extra time
-    );
+      await getDriver().wait(
+        until.elementLocated(By.xpath('//*[contains(., "Raw File Contents")]')),
+        300 // extra time
+      );
+    });
+
+    it('shows file name under active focus for screen reader', async () => {
+      const focusedHeading = await getDriver().switchTo().activeElement();
+      expect(await focusedHeading.getText()).to.equal('simple.csv');
+    });
+
+    it('shows raw file contents', async () => {
+      const rawPreview = await getDriver().findElement(By.xpath('//pre'));
+      expect(await rawPreview.getText()).to.have.string('AAAA,BBBB,CCCC,DDDD');
+    });
+
+    it('shows a preview table', async () => {
+      const tablePreview = await getDriver().findElement(By.xpath('//table'));
+
+      // header row
+      const tableCols = await tablePreview.findElements(
+        By.xpath('thead/tr/th')
+      );
+      const tableColStrings = await tableCols.reduce(
+        async (acc, col) => [...(await acc), await col.getText()],
+        Promise.resolve([] as string[])
+      );
+      expect(tableColStrings).to.deep.equal(['ColA', 'ColB', 'ColC', 'ColD']);
+
+      // first data row
+      const firstDataCells = await tablePreview.findElements(
+        By.xpath('tbody/tr[1]/td')
+      );
+      const firstDataCellStrings = await firstDataCells.reduce(
+        async (acc, col) => [...(await acc), await col.getText()],
+        Promise.resolve([] as string[])
+      );
+      expect(firstDataCellStrings).to.deep.equal([
+        'AAAA',
+        'BBBB',
+        'CCCC',
+        'DDDD'
+      ]);
+    });
+
+    it('allows toggling header row', async () => {
+      const headersCheckbox = await getDriver().findElement(
+        By.xpath(
+          '//label[contains(., "Data has headers")]/input[@type="checkbox"]'
+        )
+      );
+
+      await headersCheckbox.click();
+
+      // ensure there are no headers now
+      const tablePreview = await getDriver().findElement(By.xpath('//table'));
+      const tableCols = await tablePreview.findElements(
+        By.xpath('thead/tr/th')
+      );
+      expect(tableCols.length).to.equal(0);
+
+      // first data row should now show the header strings
+      const firstDataCells = await tablePreview.findElements(
+        By.xpath('tbody/tr[1]/td')
+      );
+      const firstDataCellStrings = await firstDataCells.reduce(
+        async (acc, col) => [...(await acc), await col.getText()],
+        Promise.resolve([] as string[])
+      );
+      expect(firstDataCellStrings).to.deep.equal([
+        'ColA',
+        'ColB',
+        'ColC',
+        'ColD'
+      ]);
+    });
   });
 }).timeout(testTimeoutMs);
