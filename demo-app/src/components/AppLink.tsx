77f6f28a59f9a5860d7993e80b6733056069d82e
Nick Matantsev <nick.matantsev@gmail.com>
2021-08-21 00:15:28 -0400

    Remove demo-app folder

--- demo-app/src/components/AppLink.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import MuiLink from '@material-ui/core/Link';
-
-export const AppLink: React.FC<{ to: string }> = (props) => {
-  return <MuiLink component={Link} underline="always" {...props} />;
-};
--- demo-app/src/pages/NotFoundPage.tsx
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-
-export const NotFoundPage: React.FC = () => (
-  <div>This page could not be found.</div>
-);
