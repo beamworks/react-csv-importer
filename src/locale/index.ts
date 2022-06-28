9a1e57c5fdf12c188bdea23663a4d095bbcbbeb8
p-multani-0 <90600160+p-multani-0@users.noreply.github.com>
2022-06-28 17:14:24 +0200

    Added Italian translations

--- src/locale/index.ts
+++ src/locale/index.ts
@@ -180,3 +180,63 @@ export const deDE: ImporterLocale = {
     processedRowsLabel: 'Verarbeitete Zeilen:'
   }
 };
+
+export const itIT: ImporterLocale = {
+  general: {
+    goToPreviousStepTooltip: 'Torna indietro'
+  },
+
+  fileStep: {
+    initialDragDropPrompt:
+      'Trascina qui il file CSV, o clicca per selezionarlo dal PC',
+    activeDragDropPrompt: 'Rilascia qui il file CSV...',
+
+    getImportError: (message) => `Errore durante l'importazione: ${message}`,
+    getDataFormatError: (message) => `Si prega di controllare il formato dei dati: ${message}`,
+    goBackButton: 'Torna indietro',
+    nextButton: 'Seleziona le colonne',
+
+    rawFileContentsHeading: 'Contenuto delfile caricato',
+    previewImportHeading: 'Anteprima dei dati',
+    dataHasHeadersCheckbox: 'Il file ha l\'intestazione',
+    previewLoadingStatus: 'Caricamento anteprima...'
+  },
+
+  fieldsStep: {
+    stepSubtitle: 'Seleziona le colonne',
+    requiredFieldsError: 'Si prega di assegnare tutte le colonne richieste',
+    nextButton: 'Importa',
+
+    dragSourceAreaCaption: 'Colonne da importare',
+    getDragSourcePageIndicator: (currentPage: number, pageCount: number) =>
+      `Page ${currentPage} of ${pageCount}`,
+    getDragSourceActiveStatus: (columnCode: string) =>
+      `Assigning column ${columnCode}`,
+    nextColumnsTooltip: 'Mostra colonna successiva',
+    previousColumnsTooltip: 'Mostra colonna precedente',
+    clearAssignmentTooltip: 'Cancella tutti gli assegnamenti delle colonne',
+    selectColumnTooltip: 'Seleziona una colonna da assegnare',
+    unselectColumnTooltip: 'Deseleziona colonna',
+
+    dragTargetAreaCaption: 'Campi richiesti',
+    getDragTargetOptionalCaption: (field) => `${field} (optional)`,
+    getDragTargetRequiredCaption: (field) => `${field} (required)`,
+    dragTargetPlaceholder: 'Trascina qui la colonna',
+    getDragTargetAssignTooltip: (columnCode: string) =>
+      `Assign column ${columnCode}`,
+    dragTargetClearTooltip: 'Cancella gli assegnamenti alla colonna',
+
+    columnCardDummyHeader: 'Campo non assegnato',
+    getColumnCardHeader: (code) => `Column ${code}`
+  },
+
+  progressStep: {
+    stepSubtitle: 'Importa',
+    uploadMoreButton: 'Carica altri dati',
+    finishButton: 'Fine',
+    statusError: 'Errore di caricamento',
+    statusComplete: 'Completato',
+    statusPending: 'Caricamento...',
+    processedRowsLabel: 'Righe processate:'
+  }
+};
